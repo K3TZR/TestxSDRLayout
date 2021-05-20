b8c624aa4fde28e738706753419089d20d24b943
Douglas Adams <douglas.adams@me.com>
2021-05-19 20:51:36 -0400

    Initial Commit

---
+++ TestxSDRLayout/TestxSDRLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestxSDRLayoutApp.swift
+//  TestxSDRLayout
+//
+//  Created by Douglas Adams on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TestxSDRLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
